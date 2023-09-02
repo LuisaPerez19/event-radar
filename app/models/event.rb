8684c1ce49e3bed8a8039c67e111ebaeea14d57a
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-09-02 16:39:36 +0100

    adding a search bar

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_category,
+  against: [ :name, :category ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
