0936b9cabfd440c078f1794ea2da8f649c82dd6d
Luisa Perez <luisa.perezw3@gmail.com>
2023-09-06 16:18:48 +0100

    fixing booking form and adding has many bookings  on event and user models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  CATEGORIES = %w[Music Education Business Food&Drink Performing&VisualArts]
   has_many_attached :photos
+  CATEGORIES = %w[Music Education Business Food&Drink Performing&VisualArts]
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_category,
