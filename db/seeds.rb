f2e7358dbb1713dd8e7975b0f091c9ab760d4d18
Luisa Perez <luisa.perezw3@gmail.com>
2023-09-08 17:18:15 +0100

    adding open uri to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require "open-uri"
+
 puts 'Deleting existing records...'
 
 # Review.delete_all
